2cd7fb78575cc591640cb03b13d73431adc0dc37
Liza Neiman <5gb.place.for.love@gmail.com>
2023-09-22 13:30:56 +0300

    segments task start

--- src/main/java/itgirls/lizaneiman/task05/_main/MainTask05.java
+++ src/main/java/itgirls/lizaneiman/task05/_main/MainTask05.java
@@ -1,11 +1,19 @@
 package _main;
 
 import service.MultiplicationService;
+import service.SegmentService;
 
 public class MainTask05 {
     public static void main(String[] args) {
+        // Multiplication table exercise
         MultiplicationService multiplicationService = new MultiplicationService();
         int[] numbers = MultiplicationService.getNumbers();
-        multiplicationService.printMultiplicationTable(numbers);
+        //multiplicationService.printMultiplicationTable(numbers);
+
+        // Segments exercise
+        SegmentService segmentService = new SegmentService();
+        segmentService.enterCoordinates();
+
+
     }
 }
