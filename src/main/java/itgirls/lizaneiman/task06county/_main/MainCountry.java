f4186cace67dad5eaa9dd7fd2da72aeff45049ba
Liza Neiman <5gb.place.for.love@gmail.com>
2023-09-28 22:53:05 +0300

    citizen service and print citizens

--- src/main/java/itgirls/lizaneiman/task06county/_main/MainCountry.java
+++ src/main/java/itgirls/lizaneiman/task06county/_main/MainCountry.java
@@ -1,7 +1,24 @@
 package _main;
 
+import model.Citizen;
+import model.Country;
+import service.CitizenService;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainCountry {
     public static void main(String[] args) {
         System.out.println("Country Exercise");
+
+        Country country = Country.getInstance();
+        CitizenService citizenService = new CitizenService();
+        List<Citizen> citizenList = new ArrayList<>();
+
+        while(Citizen.count < country.getPopulation()) {
+            Citizen citizen = citizenService.generateCitizen();
+            citizenList.add(citizen);
+            System.out.println(citizen.getID());
+        }
     }
 }
---
+++ src/main/java/itgirls/lizaneiman/task06county/service/CitizenService.java
@@ -0,0 +1,10 @@
+package service;
+
+import model.Citizen;
+
+public class CitizenService {
+    public Citizen generateCitizen() {
+        return new Citizen();
+    };
+
+}
