0c9ab4e0a5f6c6ac97f8d873e34175d64e1551d7
Liza Neiman <5gb.place.for.love@gmail.com>
2023-09-28 23:13:15 +0300

    generate citizen age

--- src/main/java/itgirls/lizaneiman/task06county/service/CitizenService.java
+++ src/main/java/itgirls/lizaneiman/task06county/service/CitizenService.java
@@ -3,8 +3,16 @@ package service;
 import model.Citizen;
 
 public class CitizenService {
+
+    private int generateAge() {
+        int minAge = 0;
+        int maxAge = 99;
+        return (int)Math.floor(Math.random() * (maxAge - minAge + 1) + minAge);
+    }
     public Citizen generateCitizen() {
-        return new Citizen();
+        Citizen citizen = new Citizen();
+
+        return citizen;
     };
 
 }
